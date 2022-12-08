d667d91a5c980dfd81b05c58230719d8132e6d2d
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2022-12-08 23:19:51 +0700

    init kategori

---
+++ lib/features/kategori/controller/kategori_remote_data_sources.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:ingatkan/core/constants/environment.dart';
+import 'package:ingatkan/core/error/error.dart';
+import 'package:ingatkan/features/authentication/model/profile.dart';
+
+abstract class KategoriRemoteDataSources {
+  Future<Profile> login({String? username, String? password});
+  Future<bool> register({String? username, String? password, String? name, String? phone, String? email});
+}
+
+class AuthenticationRemoteDataSourcesImpl implements KategoriRemoteDataSources {
+  @override
+  Future<Profile> login({String? username, String? password}) async {
+    var url = Uri.https(Environment.baseUrl, Environment.loginUrl);
+    var response = await http.post(url, body: {'username': username ?? '', 'password': password ?? ''});
+    if(response.statusCode == 401){
+      throw Error(statusCode: response.statusCode, message: "Password tidak sesuai");
+    }
+    var responseBody = await jsonDecode(response.body);
+    return Profile.fromJson(responseBody['data']);
+  }
+
+  @override
+  Future<bool> register({String? username, String? password, String? name, String? phone, String? email}) async {
+    var url = Uri.http(Environment.baseUrl, Environment.loginUrl);
+    var response = await http.post(url,
+        body: {'username': username ?? '',
+          'password': password ?? '',
+          'email': email ?? '',
+          'phone_number': phone ?? '',
+          'name': name ?? '',
+          'theme': false,
+        }).timeout(const Duration(seconds: 10));
+    if(response.statusCode == 401){
+      throw Error(statusCode: response.statusCode, message: "Username sudah tersedia");
+    } else if(response.statusCode == 200){
+      return true;
+    }
+    return false;
+  }
+
+}
---
+++ lib/features/kategori/view/presentation/login_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:ingatkan/features/authentication/view/presentation/register_page.dart';
+import 'package:ingatkan/features/authentication/view/view_model/view_model.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:ingatkan/services/navigator_service.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _username = TextEditingController(text: 'nakama_user');
+  final TextEditingController _password = TextEditingController(text: 'nakama');
+  final AuthenticationViewModel _viewModel = AuthenticationViewModel();
+
+  @override
+  void dispose() {
+    _username.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24
+        ),
+        child: Column(
+          children: [
+            const Text('Username: '),
+            const SizedBox(height: 16,),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1)
+              ),
+              height: 50,
+              width: double.infinity,
+              child: TextField(
+                controller: _username,
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan Username'
+                ),
+              ),
+            ),
+            const SizedBox(height: 16,),
+            const Text('Password: '),
+            const SizedBox(height: 16,),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1)
+              ),
+              height: 50,
+              width: double.infinity,
+              child: TextField(
+                controller: _password,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    hintText: 'Masukkan Password'
+                ),
+              ),
+            ),
+            const SizedBox(height: 48,),
+            ElevatedButton(
+                onPressed: (){
+                  if(!_viewModel.isLoading){
+                    _viewModel.login(context, username: _username.text, password: _password.text);
+                  }
+            }, child: Observer(
+              builder: (_) {
+                return Center(child: _viewModel.isLoading
+                    ? const SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: CircularProgressIndicator(color: Colors.white, strokeWidth: 4.0,))
+                    : const Text('Login'));
+              }
+            )),
+            const SizedBox(height: 16,),
+            ElevatedButton(
+                onPressed: (){
+                  NavigatorService.push(context, route: const RegisterPage());
+                }, child: const Center(child: Text('Register')))
+          ],
+        ),
+      ),
+    );
+  }
+}
