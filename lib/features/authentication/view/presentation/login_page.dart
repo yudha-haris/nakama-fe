9182124eef29cfcf1da667b65c929668d55d1ae7
Yudha Haris Purnama <yudha@clade.ventures>
2022-12-11 15:45:23 +0700

    rebuild

--- lib/features/authentication/view/presentation/login_page.dart
+++ lib/features/authentication/view/presentation/login_page.dart
@@ -29,6 +29,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Login Page'),
         centerTitle: true,
